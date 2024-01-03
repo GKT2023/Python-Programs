1e3bc0310e6f792f1cfe7d17142312b5ec74b245
GKT2023 <145818017+GKT2023@users.noreply.github.com>
2024-01-03 15:52:48 +0530

    Add files via upload

---
+++ lowerFirst.py
@@ -0,0 +1,26 @@
+'''
+Given string contains a combination of the lower and upper case letters. 
+Write a program to arrange the characters of a string so that all lowercase letters should come first.
+
+Given:
+
+str1 = PyNaTive
+
+Expected Output:
+
+yaivePNT
+'''
+
+str1 = "PyNaTive"
+
+out = ""
+for i in str1:
+    if i == i.lower():
+        out += i
+print(out)
+for i in str1:
+    if i in out:
+        continue
+    else:
+        out+= i
+print(out)
